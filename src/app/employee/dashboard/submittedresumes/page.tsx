092f828eee5df505ae0d852a47ce8f9d8d8e9a34
Narges <nargesghanbarigorji@gmail.com>
2024-01-09 23:13:59 +0330

    fix: vercel deployement error

--- src/app/employee/dashboard/submittedresumes/page.tsx
+++ src/app/employee/dashboard/submittedresumes/page.tsx
@@ -1,15 +1,22 @@
-import React from 'react';
-import { Box } from '@mui/material';
+import React from "react";
+import Box from "@mui/material/Box";
 interface Props {
   // define your props here
 }
 
 const Page: React.FC<Props> = (props) => {
   return (
-    <Box sx={{width:'90%',maxWidth:'1158px', backgroundColor:'red',marginLeft:'20%'}}>
+    <Box
+      sx={{
+        width: "90%",
+        maxWidth: "1158px",
+        backgroundColor: "red",
+        marginLeft: "20%",
+      }}
+    >
       submitted resumes
     </Box>
   );
-}
+};
 
 export default Page;
