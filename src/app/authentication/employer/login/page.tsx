a57a2f9e517239920780b42d039eddbcdc7a9771
Narges <nargesghanbarigorji@gmail.com>
2024-01-19 14:28:18 +0330

    feat: add employer login page

--- src/app/authentication/employer/login/page.tsx
+++ src/app/authentication/employer/login/page.tsx
@@ -1,48 +1,83 @@
-'use client'
+"use client";
 import React from "react";
-import { Box, Grid} from "@mui/material";
-import Link from "next/link";
-import AuthInput from "@/components/authentication/AuthInput";
-import AuthButton from "@/components/authentication/AuthButton";
-
+import Box from "@mui/material/Box";
+import { styled } from "@mui/system";
+import ButtonAppBar from "@/components/General/EmployerNavbar";
 const imageUrl = "/authentication/login.svg";
+
+const Input = styled("input")({
+  width: "507px",
+  height: "50px",
+  padding: "20px",
+  borderRadius: "5px",
+  backgroundColor: "#F9F9F9",
+  border: "solid",
+  borderWidth: "1px",
+  borderColor: "#E6E6E6",
+});
+
+const Label = styled("label")({
+  height: "24px",
+  fontSize: "14px",
+  color: "#252B42",
+  fontWeight: 500,
+  display: "block",
+});
+
 export default function Page() {
-
-
   return (
-    <Box
-      sx={{
-        width: "100vw",
-        height: "100vh",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Grid
-        container
-        justifyContent={"center"}
-        alignItems="center"
-        direction='column'
-        spacing={4}
-        
+    <>
+      <ButtonAppBar />
+      <Box
         sx={{
-          backgroundImage: `url(${imageUrl})`,
-          height: "80%",
-          width: "80%",
+          backgroundImage: `url(/General/employerAuthbackground.jpg)`,
           backgroundRepeat: "no-repeat",
-          backgroundSize: "contain",
-          backgroundPosition: "center",
+          backgroundSize: "cover",
+          opacity: "0.5",
+          height: "100vh",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
         }}
       >
-        <Grid item>
-          <AuthInput placeholder="   Username" type='string'></AuthInput>
-        </Grid>
-        <Grid item><AuthInput placeholder="   Password" type='password'></AuthInput></Grid>
-        <Grid item><Link href={'/'}>Forgot Password</Link></Grid>
-        <Grid item container justifyContent="flex-end" sx={{paddingRight:'200px'}}>  <AuthButton name={'Log in'}/></Grid>
-      </Grid>
-    
-    </Box>
+        <Box
+          sx={{
+            border: "solid",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            borderColor: "#0EC5D7",
+            padding: "15px",
+            backgroundColor: "info.contrastText",
+          }}
+        >
+          <form>
+            <Label htmlFor={"email"}>Email address</Label>
+            <Input placeholder="email" id="email" type="email" required></Input>
+            <Label htmlFor={"password"}>password</Label>
+            <Input
+              placeholder="password"
+              id="password"
+              type="password"
+              required
+            ></Input>
+            <Box sx={{display:'flex',justifyContent:'center'}}>   <input
+              type="submit"
+              value={"login"}
+              style={{
+                width: "123px",
+                height: "58px",
+                backgroundColor: "#0EC5D7",
+                border: "none",
+                color: "#FFFFFF",
+                borderRadius:'5px',
+                margin:'15px'
+              }}
+            ></input></Box>
+         
+          </form>
+        </Box>
+      </Box>
+    </>
   );
 }
